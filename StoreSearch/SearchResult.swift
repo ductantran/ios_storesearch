81a2a113a5421241f247a1e524887c271618962e
ductantran <ductan.tran95@gmail.com>
2016-11-18 09:54:05 +0700

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Tan Tran on 11/18/16.
+//  Copyright © 2016 Tan Tran. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
