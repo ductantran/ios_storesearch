13fe3d32fe74d385e2089f0bed0c8bcb390e8e13
ductantran <ductan.tran95@gmail.com>
2016-12-12 14:27:07 +0700

    App Global Tint + App Icon

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -18,6 +18,8 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchBar.becomeFirstResponder()
+        
         tableView.rowHeight = 80
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
         
